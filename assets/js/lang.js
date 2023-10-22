0e96f1b8b45408761e7813c39bd230cc30bece2a
Nodir Ergashev <nodirbek@MacBook-Pro-Nodir.local>
2023-10-22 16:59:17 +0700

    Frontend finish

---
+++ assets/js/lang.js
@@ -0,0 +1,190 @@
+const langData = {
+    "en": {
+        "home": "Home",
+        "about": "About",
+        "services": "Services",
+        "before": "Before and after",
+        "reviews": "Reviews",
+        "contact": "Contact",
+        "address": "Our address:",
+        "novosib": "Novosibirsk",
+        "our_phone": "Our telephone:",
+        "consultation4": "Order a consultation",
+        "consultation3": "Order a consultation",
+        "about_text": "A little about us",
+        "induvidual_text": "Individual approach",
+        "induvidual_text2": "We appreciate the uniqueness of each family and take into account your wishes and requirements.",
+        "widh_text": "A wide range of services",
+        "induvidual_text3": "We appreciate the uniqueness of each family and take into account your wishes and requirements.",
+        "more_detalic": "More",
+        "our_services": "All services",
+        "more_detalic2": "More",
+        "more_detalic3": "More",
+        "do_posle": "Before and after",
+        "before_1": "Befor",
+        "before_2": "After",
+        "Reviews_1": "Reviews",
+        "Heartfelt_Care": "Heartfelt Care",
+        "from_online": "Application On-Line",
+        "about_text_3": "Welcome to \"Heartfelt Care\" – your trusted companion in challenging times. We proudly offer high-quality ritual services and grave cleaning to help you honor the memory of your loved ones with care and respect.We understand that each farewell moment is unique, which is why our team is dedicated to providing you with a gentle and personalized approach. Our professionals have deep experience and sensitivity to create an atmosphere of respect and tranquility during this difficult period.",
+        "consultation_send_button": "Send",
+        "job_time_title": "Operating mode",
+        "job_time": "Every day from 9:00 to 18:00",
+        "location": "c.Novosibirsk",
+        "social_title": "Social network",
+        "powered_by": "©  2023 Powered by ",
+        "about_text5": "...We are your reliable partner in challenging times. Our company provides a wide range of funeral services and professional grave cleaning to ease your burden during difficult moments.\
+       \
+        Our mission is to care for you and your loved ones by delivering quality services while respecting your needs and traditions. We understand that each client is unique, and we are prepared to offer solutions that best align with your preferences.\
+       \
+        Our experts possess extensive experience and the sensitivity required to provide services at the highest level. We pay attention to every detail to make this period more supportive and worry-free for you.\
+        \
+        We take pride in the opportunity to stand by your side and assist during difficult times. With us, you can be confident that funeral services and grave cleaning will be performed with care and professionalism. Trust us, and we will do everything possible to lighten your load and honor the memory of your loved ones.\
+        \
+        Excellent grave cleaning! A team of professionals impressed us with their attention to detail and care. We are very satisfied with the result and highly recommend their services to everyone!...",
+        "Finding_grave": "Finding a grave",
+        "Finding_grave_text": "All you need from you is the location of the grave in the cemetery: a video or photo with landmarks...",
+        // "Finding_grave_price": "rub",
+        "corusel_key_text": " from ",
+        "corusel_key_price": "7 000",
+        "corusel_key_price_valu": "rub",
+        "repair_monument": "Repair of the monument",
+        "repair_monument_text": "All types of work on the repair of monuments: major, cosmetic.....",
+        "painting_fence": "Painting the fence",
+        "painting_text": "Our master conducts an examination of the fence at the cemetery: determines the condition, takes into account the complexity of the design, dimensions...",
+        "delete_drov": "Away from the trees",
+        "delete_text": "A modern service that expands the capabilities of relatives, regardless of current circumstances",
+        "ozeleny_title": "Landscaping of graves",
+        "ozelen_text": "You can order landscaping of any complexity from us. We take into account your wishes...",
+        "distansion": "Remote visiting",
+        "distansion_text": "A modern service that expands the capabilities of relatives, regardless of current circumstances",
+        "vozlov_color": "Laying flowers",
+        "vozlov_text": "We place fresh or artificial flowers, wreaths or baskets at the burial site on a certain date...",
+        "holma": "Hill formation",
+        "holma_text": "After burial, the grave collapses after some time, and may also undergo changes under the influence of groundwater...",
+        "rewiews_lang": "Job well done! Professional cleaning graves and tower levels. Everything was done carefully and on time. Very satisfied result. I recommend this team for any similar work. Thank you for your quality and reliability!",
+        "rewiews_lang_2": "Wonderful grave cleaning! Team of professionals I was amazed by its thoroughness and care. We are very pleased with the result and recommend it their services to everyone!", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+        "": "", 
+    
+  
+    },
+    "ru": {
+      "home": "Главная страница",
+      "about": "О нас",
+      "services": "Услуги",
+      "before": "До и После",
+      "reviews": "Отзывы",
+      "contact": "Контакты",
+      "address": "Наша адрес:",
+      "novosib": "Новосибирск",
+      "our_phone": "Наша телефон:",
+      "consultation4": "Заказать консультацию",
+      "consultation3": "Заказать консультацию",
+      "about_text": "Немного о нас",
+      "induvidual_text": "Индивидуальный подход",
+      "induvidual_text2": "Мы ценим уникальность каждой семьи и  учитываем ваши пожелания и   требования.",
+      "widh_text": "Широкий спектр услуг",
+      "induvidual_text3": "Мы ценим уникальность каждой семьи и учитываем ваши пожелания и требования.",
+      "more_detalic": "Подробнее",
+      "our_services": "Все услуги",
+      "more_detalic2": "Подробнее",
+      "more_detalic3": "Подробнее",
+      "do_posle": "До и после",
+      "before_1": "До",
+      "before_2": "После",
+      "Reviews_1": "Отзывы",
+      "Heartfelt_Care": "Сердечная Забота",
+      "from_online": "Заявка On-Line",
+      "about_text_3": "Добро пожаловать в \"Сердечная Забота\" - ваш надежный компаньон в тяжелые моменты. Мы с гордостью предоставляем высококачественные ритуальные услуги и уборку могил, чтобы помочь вам почтить память ваших близких с заботой и уважением. Мы понимаем, что каждый прощальный момент уникален, и поэтому наша команда посвящена тому, чтобы обеспечить вас ласковым и индивидуальным подходом. Наши профессионалы обладают глубоким опытом и чуткостью, чтобы создать обстановку уважения и спокойствия в этот непростой период.",
+      "consultation_send_button": "Отправить",
+      "job_time_title": "Режим работы",
+      "job_time": "Каждый день с 9:00 до 18:00",
+      "location": "г.Новосибирск",
+      "social_title": "Социальные сети",
+      "powered_by": "©  2023 Powered by ",
+      "about_text5": "Мы - ваш надежный партнер в сложные моменты. Наша компания предоставляет широкий спектр ритуальных услуг и профессиональную уборку могил, чтобы облегчить вам бремя в тяжелые моменты.\
+      \
+      Наша миссия - заботиться о вас и ваших близких, предоставляя качественные услуги в уважении к вашим потребностям и традициям. Мы понимаем, что каждый клиент уникален, и готовы предложить решения, наилучшим образом соответствующие вашим пожеланиям.\
+      \
+      Наши специалисты обладают богатым опытом и чуткостью, необходимыми для оказания услуг на высшем уровне. Мы уделяем внимание каждой детали, чтобы сделать этот период более поддерживающим и беспечным для вас.\
+      \
+      Мы гордимся возможностью быть рядом с вами и помочь в трудные времена. С нами вы можете быть уверены, что ритуальные услуги и уборка могил будут выполнены с заботой и профессионализмом. Доверьтесь нам, и мы сделаем все возможное, чтобы облегчить вашу ношу и уважить память ваших близких",
+      "Finding_grave": "Поиск могилы",
+      "Finding_grave_text": "От вас понадобится только место расположения могилы на кладбище: видео или фото с ориентирами...",
+      // "Finding_grave_price": "руб",
+      // "Finding_grave_price2": "7 000",
+      "corusel_key_text": "от",
+      "corusel_key_price": "7 000",
+      "corusel_key_price_valu": "",
+      "corusel_key_price_valu": "руб",
+      "repair_monument": "Ремонт памятника",
+      "repair_monument_text": "Все виды работ по ремонту памятников: капитальный, косметический.....",
+      "painting_fence": "Покраска ограды",
+      "painting_text": "Наш мастер проводит экспертизу ограды на кладбище: определяет состояние, учитывает сложность рисунка, габариты..",
+      "delete_drov": "Удаене деревьев",
+      "delete_text": "Все хлопоты по получению разрешения у администрации кладбища берем на себя...",
+      "ozeleny_title": "Озеление могил",
+      "ozelen_text": "Заказать озеленение любой сложности можно у нас. Учитываем ваши пожелания...",
+      "distansion": "Дистанционное посещение",
+      "distansion_text": "Современная услуга, расширяющая возможности родственников, независимо от сложившихся обстоятельств",
+      "vozlov_color": "Возложение цветов",
+      "vozlov_text": "Возлагаем живые или искусственные цветы, венки или корзины на место захоронения к определенной дате...",
+      "holma": "Формирование холма",
+      "holma_text": "После погребения могила проваливается спустя некоторое время, а также под воздействием грунтовых вод может подвергаться изменениям...",
+      "rewiews_lang": "Отлично выполненная работа! Профессиональная уборка  могил и уровней вышек. Все было сделано аккуратно и в срок. Очень доволен результатом. Рекомендую эту команду для любых подобных работ. Спасибо за ваше  качество и надежность!",
+      "rewiews_lang_2": "Замечательная уборка могил! Команда профессионалов поразила своей тщательностью и уходом. Очень довольны результатом и рекомендуем их услуги всем!",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      
+  
+    }
+  };
+  
+  const langElements = document.querySelectorAll('.lang');
+  
+  function changeLanguage(language) {
+    selectedLanguage = language;
+    langElements.forEach(el => {
+      el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+    });
+  }
+  
+  
+  
+  // Tilni qayta belgilash
+  let selectedLanguage = 'en';
+  langElements.forEach(el => {
+    el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+  });
+  
+  const langButtons = document.querySelectorAll('.lang-button');
+  langButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    changeLanguage(btn.dataset.lang);
+    langButtons.forEach(btn => {
+      btn.classList.remove('active_lang');
+    });
+    btn.classList.add('active_lang');
+  });
+  });
+  
